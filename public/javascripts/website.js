37cd4b6d1db3f0d9cab9d57ee886f49619d7eb1a
Anna <aniamagdaszymkowiak@gmail.com>
2022-01-31 22:29:23 +0100

    a huge update

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,4 +1,5 @@
 const list = document.querySelector("#words");
+const counter = document.getElementById("counter");
 let words = [];
 let copy = [];
 
@@ -25,26 +26,53 @@ function showWords(){
         element.innerHTML = word;
         list.appendChild(element);
     }
-
+    let n = list.childNodes.length;
+    counter.innerHTML = `Number of words: ${n}`
 }
 
 function search(){
     let start = document.getElementById("start").value;
-    document.getElementById("start").value = '';
     let end = document.getElementById("end").value;
-    document.getElementById("end").value = '';
+    let con = document.getElementById("con").value;
+    let not = document.getElementById("not").value;
+    let length = document.getElementById("length").value;
     let pos = document.getElementById("pos").value;
-    document.getElementById("pos").value = '';
-    let length = parseInt(document.getElementById("length").value);
-    document.getElementById("length").value = '';
     reset();
     if(start !== ''){
-        words = words.filter(word => word.charAt(0) === start);
+        let n = start.length;
+        words = words.filter(word => word.substring(0, n) === start);
+    }
+    if(end !== ''){
+        let n = end.length;
+        words = words.filter(word => word.substring(word.length - n) === end);
     }
     if(length !== ''){
+        length = parseInt(length);
         words = words.filter(word => word.length === length);
     }
+    if(not !== ''){
+        for(i=0;i<not.length;i++){
+            words = words.filter(word => !word.includes(not.charAt(i)));
+        }
+    }
+    if(con !== ''){
+        for(i=0;i<con.length;i++){
+            words = words.filter(word => word.includes(con.charAt(i)));
+        }
+    }
+    if(pos !== ''){
+        words = words.filter(word => matchWord(word, pos));
+    }
     showWords();
+}
+
+function matchWord(word, pattern){
+	let ret = true
+	pattern.split("").forEach((c, i) => {
+		if (c != "_" && word[i] != c)
+			ret = false
+	})
+	return ret
 }
 
 function reset(){
@@ -54,4 +82,13 @@ function reset(){
     let n = list.childNodes.length;
     for(i = 0;i<n;i++)
         list.removeChild(list.childNodes[0]);
+}
+
+function resetInput() {
+    document.getElementById("start").value = '';
+    document.getElementById("end").value = '';
+    document.getElementById("con").value = '';
+    document.getElementById("not").value = '';
+    document.getElementById("length").value = '';
+    document.getElementById("pos").value = '';
 }
